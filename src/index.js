9f052936d5c6c776b8f18840888bf64e69eb608a
achref-benahmed <achref.benhmed@gmail.com>
2018-12-15 03:00:08 +0100

    component first template

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
-import React from 'react' ;
-import ReactDOM from 'react-dom' ;
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 const App = () => {
-return <div> Hello </div>
+return (
+  <div className="ui container comments">
+  <div className="comment">
+    <a href="/" className="avatar" >
+      <img alt="avatar" />
+    </a>
+    <div className="content">
+      <a href="/" className="author">
+    Achraf </a>
+      <div className="metadata">
+        <span className="date"> 14.12 .2018 < /span> </div>
+        <div className = "post"   >
+    Nice!
+    </div >
+
+     </div>
+   </div >
+ </div>
+
+
+);
 
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render( < App / > , document.querySelector('#root'));
